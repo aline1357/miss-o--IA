c0906a92200794f1358e9add86de28dbd47b6297
aline1357 <maria.silva.aline@escola.pr.gov.br>
2026-07-27 14:21:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,48 @@ function mostraPergunta() {
   caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+const alternativa
+(const alternativa of perguntaAtual.alternativas)
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+  }
+}
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+  }
+}
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+  }
+}
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
